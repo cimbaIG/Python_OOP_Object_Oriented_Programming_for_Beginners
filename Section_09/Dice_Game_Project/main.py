d2fa0c3de34d67d41d1593b93f3c18e8228ef575
cimbaIG <mihael.cindori@gmail.com>
2023-03-10 21:19:53 +0100

    Implement Player() class and its methods

--- Section_09/Dice_Game_Project/main.py
+++ Section_09/Dice_Game_Project/main.py
@@ -24,11 +24,43 @@ class Die:
         new_value = random.randint(1, 6)
         self._value = new_value
         return new_value
-    
-    
-# Testing the class Die()
-die = Die()    
-print(die.value)
 
-die.roll()
-print(die.value)
+
+class Player:
+    
+    def __init__(self, die, is_computer=False):
+        self._die = die
+        self._is_computer = is_computer
+        self._counter = 10
+
+    # Getter for die attribute
+    @property
+    def die(self):
+        return self._die
+    
+    # Getter for is_computer attribute
+    @property
+    def is_computer(self):
+        return self._is_computer
+    
+    # We provide only a getter (without a setter) as we do not want
+    # to alter the counter value outside the class. We want to do 
+    # that only inside the class and this is a reason why we set
+    # counter attribute as non-public.
+    @property
+    def counter(self):
+        return self._counter
+    
+    # Increment counter attribute by 1
+    def increment_counter(self):
+        self._counter += 1
+        
+    # Decrement counter attribute by 1
+    def decrement_counter(self):
+        self._counter -= 1
+    
+    # Aggregation in action
+    # First get a die instance by calling self._die and after that
+    # call the roll() method defined inside a Die() class.
+    def roll_die(self):
+        return self._die.roll()
