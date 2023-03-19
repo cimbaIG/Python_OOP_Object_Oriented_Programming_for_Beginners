47847a509d05d2f4c45c54c7ad08c4992a11e75f
cimbaIG <mihael.cindori@gmail.com>
2023-03-19 18:11:51 +0100

    War card game - Implement Suit class

---
+++ Section_19_Bonus_section/War_Card_Game/main.py
@@ -0,0 +1,6 @@
+from suit import Suit
+
+
+# Testing Suit class
+my_suit = Suit("SpAdes")
+print(my_suit.description, my_suit.symbol)
---
+++ Section_19_Bonus_section/War_Card_Game/suit.py
@@ -0,0 +1,23 @@
+class Suit:
+    
+    suits = {
+        "clubs": "♣",
+        "diamonds": "♦",
+        "hearts": "♥",
+        "spades": "♠"
+    }
+    
+    def __init__(self, description: str):
+        self._description = description.lower()
+        try:
+            self._symbol = Suit.suits[self._description]
+        except KeyError:
+            print("Legit suits are: clubs, diamonds, hearts and spades.")
+        
+    @property
+    def description(self):
+        return self._description
+    
+    @property
+    def symbol(self):
+        return self._symbol
