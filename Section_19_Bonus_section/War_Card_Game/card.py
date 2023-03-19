06e4f228c30dd4204a8cb816b62b7c1b6ec01b07
cimbaIG <mihael.cindori@gmail.com>
2023-03-19 18:42:32 +0100

    War card game - Implement Card class

---
+++ Section_19_Bonus_section/War_Card_Game/card.py
@@ -0,0 +1,38 @@
+from suit import Suit
+
+
+class Card:
+    
+    special_cards = {
+        11: "Jack",
+        12: "Queen",
+        13: "King",
+        14: "Ace"
+    }
+    
+    def __init__(self, suit: Suit, value: int):
+        self._suit = suit
+        self._value = value
+        
+    @property
+    def suit(self):
+        return self._suit
+    
+    @property
+    def value(self):
+        return self._value
+    
+    def _is_special(self):
+        if self.value >= 11:
+            return True
+        return False
+    
+    def show(self):
+        if self._is_special():
+            print(f"Card value: {Card.special_cards[self.value]}\n"
+                  + f"Card suit: {self.suit.description.capitalize()}" 
+                  + f" {self.suit.symbol}")
+        else:
+            print(f"Card value: {self.value}\n"
+                  + f"Card suit: {self.suit.description.capitalize()}" 
+                  + f" {self.suit.symbol}")
--- Section_19_Bonus_section/War_Card_Game/main.py
+++ Section_19_Bonus_section/War_Card_Game/main.py
@@ -1,6 +1,10 @@
 from suit import Suit
+from card import Card
 
 
 # Testing Suit class
 my_suit = Suit("SpAdes")
 print(my_suit.description, my_suit.symbol)
+
+my_card = Card(suit=my_suit, value=11)
+my_card.show()
